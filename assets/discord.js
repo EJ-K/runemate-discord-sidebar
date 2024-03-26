023d683a8b8a76cc26243c06093e7057dcb4bfbc
Party <party@runemate.com>
2024-03-26 16:31:57 +0000

    make the join text optional

--- assets/discord.js
+++ assets/discord.js
@@ -2,26 +2,30 @@ var serverID = getParameterByName('serverID');
 var title = getParameterByName('title') ? getParameterByName('title') : false;
 var invite = getParameterByName('invite') ? getParameterByName('invite') : false;
 var theme = getParameterByName('theme') ? getParameterByName('theme') : 'dark';
+var showText = getParameterByName("showText") ? getParameterByName("showText") === "true" : false
 
 $.getJSON('https://discordapp.com/api/servers/' + serverID + '/widget.json', function (data) {
     $("head").append('<link rel="stylesheet" href="assets/' + theme + '.css" />');
     $('.widgetHeaderCount').html('<strong>' + data.presence_count + '</strong> members online!');
 
-    var titlebar = `<span class="widgetFooterText">Join the `;
-    if (title) {
-        titlebar += `<strong>` + title + `</strong>`;
-    } else {
-        titlebar += `<strong>` + data.name + `</strong>`;
+    var wrapper = $('.widgetFooter');
+    var titlebar = ``;
+    if (showText) {
+        titlebar = `Join the `;
+        if (title) {
+            titlebar += `<strong>` + title + `</strong>`;
+        } else {
+            titlebar += `<strong>` + data.name + `</strong>`;
+        }
+
+        titlebar += " Discord community!"
     }
 
-    titlebar += " Discord community!</span>"
-    var wrapper = $('.widgetFooter');
-    wrapper.append(titlebar);
+    wrapper.append('<span class="widgetFooterText">' + titlebar + '</span>');
     if (invite) {
         var text = `<a class="widgetBtnConnect" href="` + data.instant_invite + `" target="_blank">Join Discord</a>`;
         wrapper.append(text);
     }
-
 });
 
 
